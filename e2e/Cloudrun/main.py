804d5dd6d8bf0247f8c29ee4a7e9ce59843820b6
Akshata Madavi <akshatamadavi@gmail.com>
2025-04-26 21:51:26 +0530

    update e2e

--- e2e/Cloudrun/main.py
+++ e2e/Cloudrun/main.py
@@ -12,14 +12,24 @@ def read_file():
     filename = request_data.get("filename")
     filepath = request_data.get("filepath")
     project = request_data.get("project")
+    dataset = request_data.get("dataset")
+    table = request_data.get("table")
+
+    table_id = project+'.'+dataset+'.'+table
 
     bucket_name = filepath.split('/')[0]
     blob_ame = '/'.join(filepath.split('/')[0])+filename
     ## read data from GCS bucket
-    data = read_gcs_file_as_json(bucket_name, blob_name)
-    
+    # data = read_gcs_file_as_json(bucket_name, blob_name)
+    data = pd.read_csv('gs://'+filepath+'/'+filename)
+    pandas_gbq.to_gbq(
+    data,
+    table_id,
+    project_id=project,
+    if_exists="append"  # Options: 'fail', 'replace', 'append'
+)
 
-    return 'Hello, World!'
+    return 200
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
